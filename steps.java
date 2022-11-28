8ec414eec5a4254cb2a218298b6c24647cafcc49
Ujjwal kr. Singh <94126186+Ujjwal-kr-Singh@users.noreply.github.com>
2022-11-28 23:05:00 +0530

    Add files via upload

---
+++ steps.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class steps {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println(calstpes(n)); 
+    }
+static int c =1;
+    private static int calstpes(int n) {
+        if(n<=1)
+        return c;
+        c=c +1;
+        calstpes(n-1);
+        calstpes(n-2);
+    return c;
+    }
+}
